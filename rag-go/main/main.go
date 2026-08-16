28d8229fcc7a7b7fe0bb4d799c0a2edef039bc10
Manan patel <immnan333@gmail.com>
2026-08-16 18:02:19 +0530

    version

--- rag-go/main/main.go
+++ rag-go/main/main.go
@@ -25,7 +25,7 @@ func main() {
 
 	// --- Config from env ---
 	cfg := loadConfig()
-	orcaVersion := "0.5.13"
+	orcaVersion := "0.6+"
 	log.Info().
 		Str("port", cfg.FiberPort).
 		Str("qdrant_host", cfg.QdrantHost).
